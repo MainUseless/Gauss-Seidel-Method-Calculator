63861946d8c12bd4fe616f9edaa5955449310cba
MainUseless <mostafa777444333@gmail.com>
2021-12-20 15:33:16 +0200

    update

--- code.js
+++ code.js
@@ -1,7 +1,27 @@
+function Start(){
+    document.getElementById("cover").style.display = "none";
+    document.getElementById("calculator").style.display = "block";
+    document.getElementById("instruct").style.display = "block";   
+}
+
+function resetAll(){
+    reset();
+    document.getElementById("cover").style.display = "block";
+    document.getElementById("calculator").style.display = "none";
+    document.getElementById("instruct").style.display = "none";  
+
+}
+
 function calculate() {
     removeAllChildNodes(document.getElementById("answer"))
     let tempArray=getInputFrom("box");
     console.log(tempArray);
+    if(tempArray.length<1)
+    {
+     printOnPage("Empty matrix")
+     return 1;
+    }
+
     if(Math.sqrt(tempArray.length) % 1 !== 0){
      printOnPage("Matrix must be square")
      return 1;
@@ -21,7 +41,7 @@ function calculate() {
     for (let i=0 ; i<tempMatrix.length;i++)       //to reorder the matrix 
     {
     let index = findPlace(tempMatrix[i]) ;
-     if(index==-1){
+     if(index===-1){
         printOnPage("Error in row "+(i+1));
         return 1;
      }
@@ -30,7 +50,7 @@ function calculate() {
     }
 
     for(let i=0 ; i<solutionBox.length ; i++)
-    if(solutionBox[i]==-1)
+    if(solutionBox[i]===-1)
     {
         printOnPage("Input matrix contains some error")
         return 1;
@@ -92,10 +112,11 @@ function chunk(matrix) {
     let seed=document.getElementById("seed").value;
     let n = M.length;
     let X = new Array(n).fill(parseInt(seed)); // Approximations
-    let P = new Array(n); // Prev
-
+    let P = new Array(n).fill(parseInt(seed)); // Prev
+    
     for (let k=0 ; k<counter ;k++) 
     {
+        //if(k+1==counter)for (let i=0 ; i<n ; i++) P[i]=X[i];
         for (let i = 0; i < n; i++) 
         {
             let sum = S[i]; // b_n
@@ -105,10 +126,21 @@ function chunk(matrix) {
     // Update x_i to use in the next row calculation
             X[i] = 1/M[i][i] * sum;
         }
-        P = X;
-    //    if (Math.abs(X[i] - P[i]) > epsilon) break;
+        //P = X;
     };
-    for(let i=0 ; i< X.length ; i++)
+    let L = new Array(n).fill(" ");
+    //console.log(P);
+    //console.log(X);
+    for(let i=0 ; i< X.length ; i++){
+        for(let j=0 ; j< X.length ; j++)
+            {
+             L[i]+=M[i][j]+"(X"+(j+1)+")";
+            if(j+1!=X.length)
+              L[i]+="+ ";
+            }
+        //console.log(L[i]);
+        printOnPage("X"+(i+1)+" = "+"1/"+M[i][i]+" * ("+S[i]+" - ("+L[i]+")");}
+    for(let i=0 ; i< X.length ; i++)  
         printOnPage("X"+(i+1)+" = "+X[i].toFixed(4));
 }
 
@@ -120,6 +152,7 @@ function printOnPage(text){
 }
 
 function reset(){
+    document.getElementById("answer").style.display = "none";
     removeAllChildNodes(document.getElementById("answer"));
     document.getElementById("box").value=null;
     document.getElementById("solution").value=null;
